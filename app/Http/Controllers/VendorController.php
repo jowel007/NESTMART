384eec747f5201752881537bea342fe904476b0c
jowel007 <abuhanifjowel1@gmail.com>
2022-12-26 23:02:28 +0600

    vendor logout option

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -3,10 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class VendorController extends Controller
 {
     public function VendorDashboard(){
         return view('vendor.index');
+    }
+
+    public function VendorLogin(){
+        return view('vendor.vendor_login');
+    }
+
+    public function VendorDestroy(Request $request){
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+        return redirect('/vendor/login');
     }
 }

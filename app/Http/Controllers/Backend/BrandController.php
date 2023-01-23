8239599a36480a40dace3649e058b2e0e0120957
jowel007 <abuhanifjowel1@gmail.com>
2023-01-23 23:11:17 +0600

    brand crud insert - I

--- app/Http/Controllers/Backend/BrandController.php
+++ app/Http/Controllers/Backend/BrandController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use App\Models\Brand;
 use Illuminate\Http\Request;
+use Image;
 
 class BrandController extends Controller
 {
@@ -13,8 +14,28 @@ class BrandController extends Controller
         return view('backend.brand.brand_all',compact('brand'));
     }
 
-    public function AddBrand(Request $request)
+    public function AddBrand()
     {
         return view('backend.brand.add_brand');
     }
+
+    public function StoreBrand(Request $request){
+        $image = $request->file('brand_image');
+        $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+        Image::make($image)->resize(300,300)->save('upload/brand/'.$name_gen);
+        $save_url = 'upload/brand/'.$name_gen;
+
+        Brand::insert([
+            'brand_name' => $request->brand_name,
+            'brand_slug' => strtolower(str_replace(' ', '-',$request->brand_name)),
+            'brand_image' => $save_url,
+        ]);
+
+        $notification = array(
+            'message' => 'Brand Inserted Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('all.brand')->with($notification);
+    }  //end method
 }

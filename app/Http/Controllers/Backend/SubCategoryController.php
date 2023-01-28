7cbc450f9c99877ca783eb26e3c88835a3af14f8
jowel007 <abuhanifjowel1@gmail.com>
2023-01-28 19:07:11 +0600

    add subcategory and fetch category in category table id wise

--- app/Http/Controllers/Backend/SubCategoryController.php
+++ app/Http/Controllers/Backend/SubCategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use App\Models\SubCategory;
 use Illuminate\Http\Request;
 
@@ -11,5 +12,12 @@ class SubCategoryController extends Controller
     public function AllSubCategory(){
         $subcategories = SubCategory::latest()->get();
         return view('backend.subcategory.subcategory_all',compact('subcategories'));
+    }  // end method
+
+    public function AddSubCategory(){
+        $categories = Category::orderBy('category_name','ASC')->get();
+        return view('backend.subcategory.add_subcategory',compact('categories'));
     }
+
+
 }
--- app/Models/SubCategory.php
+++ app/Models/SubCategory.php
@@ -10,4 +10,9 @@ class SubCategory extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+
+    public function Category(){
+        return $this->belongsTo(Category::class,'category_id','id');
+    }
 }

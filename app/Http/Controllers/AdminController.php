e2a3152dd60eb9119cb1c2978d4e098c6badcff5
jowel007 <abuhanifjowel1@gmail.com>
2023-03-01 21:46:46 +0600

    active vendor to inactive

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -98,12 +98,12 @@ class AdminController extends Controller
     public function ActiveVendor(){
         $ActiveVendor = User::where('status','active')->where('role','vendor')->latest()->get();
         return view('backend.vendor.active_vendor',compact('ActiveVendor'));
-    }
+    }// End Mehtod 
 
     public function InActiveVendorDetails($id){
         $inactiveVendorDetails = User::findOrFail($id);
         return view('backend.vendor.inactive_vendor_details',compact('inactiveVendorDetails'));
-    }
+    }// End Mehtod
 
     public function ActiveVendorApprove(Request $request){
 
@@ -119,7 +119,28 @@ class AdminController extends Controller
 
         return redirect()->route('active.vendor')->with($notification);
 
-    }
+    }// End Mehtod
+
+    public function ActiveVendorDetails($id){
+        $activeVendorDetails = User::findOrFail($id);
+        return view('backend.vendor.active_vendor_details',compact('activeVendorDetails'));
+    }// End Mehtod
+
+    public function InActiveVendorApprove(Request $request){
+
+        $verdor_id = $request->id;
+        $user = User::findOrFail($verdor_id)->update([
+            'status' => 'inactive',
+        ]);
+
+        $notification = array(
+            'message' => 'Vendor InActive Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('inactive.vendor')->with($notification);
+
+    }// End Mehtod
 
 
 }

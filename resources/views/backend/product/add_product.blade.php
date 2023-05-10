2d31486988766ed4258e1e0129da13bebc30606f
jowel007 <abuhanifjowel1@gmail.com>
2023-05-11 00:02:41 +0600

    add validation in add product page

--- resources/views/backend/product/add_product.blade.php
+++ resources/views/backend/product/add_product.blade.php
@@ -25,36 +25,41 @@
             <div class="card-body p-4">
                 <h5 class="card-title">Add New Product</h5>
                 <hr/>
+
+
+                <form id="myForm" method="post" action="{{ route('store.category') }}" enctype="multipart/form-data" >
+                    @csrf
+
                 <div class="form-body mt-4">
                     <div class="row">
                         <div class="col-lg-8">
                             <div class="border border-3 p-4 rounded">
-                                <div class="mb-3">
+                                <div class="form-group mb-3">
                                     <label for="inputProductTitle" class="form-label">Product Name</label>
                                     <input type="text" name="product_name" class="form-control" id="inputProductTitle" placeholder="Enter product title">
                                 </div>
-                                <div class="mb-3">
+                                <div class="form-group mb-3">
                                     <label for="inputProductTitle" class="form-label">Product Tags</label>
                                     <input type="text" name="product_tags" class="form-control visually-hidden" data-role="tagsinput" value="New product,Top Product,Feture Product">
                                 </div>
-                                <div class="mb-3">
+                                <div class="form-group mb-3">
                                     <label for="inputProductTitle" class="form-label">Product Size</label>
                                     <input type="text" name="product_size" class="form-control visually-hidden" data-role="tagsinput" value="M,L,XL,XXL">
                                 </div>
-                                <div class="mb-3">
+                                <div class="form-group mb-3">
                                     <label for="inputProductTitle" class="form-label">Product Color</label>
                                     <input type="text" name="product_color" class="form-control visually-hidden" data-role="tagsinput" value="Black,Red,Green,Off White,Blue,Dark green">
                                 </div>
-                                <div class="mb-3">
+                                <div class="form-group mb-3">
                                     <label for="inputProductDescription" class="form-label">Sort Description</label>
                                     <textarea class="form-control" name="short_descp" id="inputProductDescription" rows="3"></textarea>
                                 </div>
-                                <div class="mb-3">
+                                <div class="form-group mb-3">
                                     <label for="inputProductDescription" class="form-label">Long Description</label>
                                     <textarea id="mytextarea" name="long_descp">Hello, World!</textarea>
                                 </div>
 
-                                <div class="mb-3">
+                                <div class="form-group mb-3">
                                     <label for="inputProductTitle" class="form-label">Main Thambnail</label>
                                     <input class="form-control" onChange="mainThamUrl(this)" name="product_thambnail" type="file" id="formFile">
                                     <img src="" id="mainThamb" />
@@ -62,7 +67,7 @@
 
 
 
-                                <div class="mb-3">
+                                <div class="form-group mb-3">
                                     <label for="inputProductTitle" class="form-label">Multiple Image</label>
 
                                     <input class="form-control" name="multi_img[]" type="file" id="multiImg" multiple="">
@@ -76,24 +81,24 @@
                         <div class="col-lg-4">
                             <div class="border border-3 p-4 rounded">
                                 <div class="row g-3">
-                                    <div class="col-md-6">
+                                    <div class="form-group col-md-6">
                                         <label for="inputPrice" class="form-label">Product Price</label>
                                         <input type="text" name="selling_price" class="form-control" id="inputPrice" placeholder="00.00">
                                     </div>
-                                    <div class="col-md-6">
+                                    <div class="form-group col-md-6">
                                         <label for="inputCompareatprice" class="form-label">Discount Price</label>
                                         <input type="text" class="form-control" id="inputCompareatprice" placeholder="00.00">
                                     </div>
-                                    <div class="col-md-6">
+                                    <div class="form-group col-md-6">
                                         <label for="inputCostPerPrice" class="form-label">Product Code</label>
                                         <input type="text" name="product_code" class="form-control" id="inputCostPerPrice" placeholder="00.00">
                                     </div>
-                                    <div class="col-md-6">
+                                    <div class="form-group col-md-6">
                                         <label for="inputStarPoints" class="form-label">Product Quantity</label>
                                         <input type="text" name="product_qty" class="form-control" id="inputStarPoints" placeholder="00.00">
                                     </div>
 
-                                    <div class="col-12">
+                                    <div class="form-group col-12">
                                         <label for="inputProductType" class="form-label">Product Brand</label>
                                         <select name="brand_id" class="form-select" id="inputProductType">
                                             <option></option>
@@ -103,7 +108,7 @@
                                         </select>
                                     </div>
 
-                                    <div class="col-12">
+                                    <div class="form-group col-12">
                                         <label for="inputVendor" class="form-label">Product Category</label>
                                         <select name="category_id" class="form-select" id="inputVendor">
                                             <option></option>
@@ -112,7 +117,7 @@
                                             @endforeach
                                         </select>
                                     </div>
-                                    <div class="col-12">
+                                    <div class="form-group col-12">
                                         <label for="inputCollection" class="form-label">Product SubCategory</label>
                                         <select name="subcategory_id" class="form-select" id="inputCollection">
                                             <option></option>
@@ -120,7 +125,7 @@
                                         </select>
                                     </div>
 
-                                    <div class="col-12">
+                                    <div class="form-group col-12">
                                         <label for="inputCollection" class="form-label">Select Vendor</label>
                                         <select name="vendor_id" class="form-select" id="inputCollection">
                                             <option></option>
@@ -133,13 +138,13 @@
 
                                     <div class="col-12">
                                         <div class="row g-3">
-                                            <div class="col-md-6">
+                                            <div class="form-group col-md-6">
                                                 <div class="form-check form-switch">
                                                     <input class="form-check-input" name="hot_deals" value="1" type="checkbox" id="flexSwitchCheckChecked" checked="">
                                                     <label class="form-check-label" for="flexSwitchCheckChecked">Hot Deals</label>
                                                 </div>
                                             </div>
-                                            <div class="col-md-6">
+                                            <div class="form-group col-md-6">
                                                 <div class="form-check form-switch">
                                                     <input class="form-check-input" name="featured" value="1" type="checkbox" id="flexSwitchCheckChecked" checked="">
                                                     <label class="form-check-label" for="flexSwitchCheckChecked">Featured</label>
@@ -148,14 +153,14 @@
 
                                             <hr>
 
-                                            <div class="col-md-6">
+                                            <div class="form-group col-md-6">
                                                 <div class="form-check form-switch">
                                                     <input class="form-check-input" name="special_offer" value="1" type="checkbox" id="flexSwitchCheckChecked" checked="">
                                                     <label class="form-check-label" for="flexSwitchCheckChecked">Special Offer</label>
                                                 </div>
                                             </div>
 
-                                            <div class="col-md-6">
+                                            <div class="form-group col-md-6">
                                                 <div class="form-check form-switch">
                                                     <input class="form-check-input" name="special_deals" value="1" type="checkbox" id="flexSwitchCheckChecked" checked="">
                                                     <label class="form-check-label" for="flexSwitchCheckChecked">Special Deal</label>
@@ -169,7 +174,7 @@
                                     </div>
                                     <div class="col-12">
                                         <div class="d-grid">
-                                            <button type="button" class="btn btn-primary">Save Product</button>
+                                            <input type="submit" class="btn btn-primary px-4" value="Save Changes" />
                                         </div>
                                     </div>
                                 </div>
@@ -177,6 +182,9 @@
                         </div>
                     </div><!--end row-->
                 </div>
+
+                </form>
+
             </div>
         </div>
 
@@ -251,6 +259,83 @@
             }
         });
     });
+</script>
+
+
+ {{--    validation --}}
+
+ <script type="text/javascript">
+    $(document).ready(function (){
+        $('#myForm').validate({
+            rules: {
+                product_name: {
+                    required : true,
+                },
+                short_descp: {
+                    required : true,
+                }, 
+                 product_thambnail: {
+                    required : true,
+                }, 
+                 multi_img: {
+                    required : true,
+                }, 
+                 selling_price: {
+                    required : true,
+                },                   
+                 product_code: {
+                    required : true,
+                }, 
+                 product_qty: {
+                    required : true,
+                }, 
+                 brand_id: {
+                    required : true,
+                }, 
+                 category_id: {
+                    required : true,
+                }, 
+                 subcategory_id: {
+                    required : true,
+                }, 
+            },
+            messages :{
+                product_name: {
+                    required : 'Please Enter Product Name',
+                },
+                short_descp: {
+                    required : 'Please Enter Short Description',
+                },
+                product_thambnail: {
+                    required : 'Please Select Product Thambnail Image',
+                },
+                multi_img: {
+                    required : 'Please Select Product Multi Image',
+                },
+                selling_price: {
+                    required : 'Please Enter Selling Price',
+                }, 
+                product_code: {
+                    required : 'Please Enter Product Code',
+                },
+                 product_qty: {
+                    required : 'Please Enter Product Quantity',
+                },
+            },
+            errorElement : 'span', 
+            errorPlacement: function (error,element) {
+                error.addClass('invalid-feedback');
+                element.closest('.form-group').append(error);
+            },
+            highlight : function(element, errorClass, validClass){
+                $(element).addClass('is-invalid');
+            },
+            unhighlight : function(element, errorClass, validClass){
+                $(element).removeClass('is-invalid');
+            },
+        });
+    });
+
 </script>
 
 @endsection

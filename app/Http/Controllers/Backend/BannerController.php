f281d070b3ff05176943913962988c473603d63f
jowel007 <abuhanifjowel1@gmail.com>
2023-07-08 22:55:53 +0600

    banner edit and update

--- app/Http/Controllers/Backend/BannerController.php
+++ app/Http/Controllers/Backend/BannerController.php
@@ -41,4 +41,57 @@ class BannerController extends Controller
     }  // End Method
 
 
+    public function EditBanner($id){
+        $banners = Banner::findOrFail($id);
+        return view('backend.banner.edit_banner',compact('banners'));
+    }
+
+    public function UpdateBanner(Request $request){
+        $banner_id = $request->id;
+        $old_img = $request->old_image;
+
+        if ($request->file('banner_image')) {
+
+        $image = $request->file('banner_image');
+         $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+        Image::make($image)->resize(768,450)->save('upload/banner/'.$name_gen);
+        $save_url = 'upload/banner/'.$name_gen;
+
+        if (file_exists($old_img)) {
+           unlink($old_img);
+        }
+
+        Banner::findOrFail($banner_id)->update([
+            'banner_title' => $request->banner_title,
+            'banner_url' => $request->banner_url,
+            'banner_image' => $save_url, 
+        ]);
+
+       $notification = array(
+            'message' => 'Banner Updated with image Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('all.banner')->with($notification); 
+
+        } else {
+
+            Banner::findOrFail($banner_id)->update([
+            'banner_title' => $request->banner_title,
+            'banner_url' => $request->banner_url, 
+        ]);
+
+       $notification = array(
+            'message' => 'Banner Updated without image Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('all.banner')->with($notification); 
+
+        } // end else
+        
+    }
+
+
+
 }
